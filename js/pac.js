910b15f77e8bf428aa6f1db7ae7db17f140f15b7
rmirma <115686470+rmirma@users.noreply.github.com>
2022-12-04 15:29:55 +0200

    comments
    Fail kommenteeritud

--- js/pac.js
+++ js/pac.js
@@ -1,18 +1,21 @@
-
+        /*
+         * Pacman by @ramziosta
+         * Credit: ramziosta.github.io/
+         */
 const pacArray = [
     ['https://ramziosta.github.io/Pacman/images/PacMan1.png', 'https://ramziosta.github.io/Pacman/images/PacMan2.png'],['https://ramziosta.github.io/Pacman/images/PacMan3.png', 'https://ramziosta.github.io/Pacman/images/PacMan4.png']
 ];
 var focus = 0;
-const pacMen = [];  // This array holds all the pacmen
+const pacMen = [];  // See list hoiustab kõik lisatavad PuckManid
 
-// This function returns an object with random values
+// Funktsioon tagastab objekti suvaliste väärtustega
 function setToRandom(scale) {
     return {
             x: Math.random() * scale, y: Math.random() * scale,
     };
 }
 
-function makePac() {
+function makePac() { // See funktsioon vastutab PuckMani isendi valmimise eest
     let velocity = setToRandom(13); // {x:?, y:?}
     let position = setToRandom(800);
     let direction = 0;
@@ -29,7 +32,7 @@ function makePac() {
     };
 }
 
-function update() {
+function update() { // Funktsioon paneb loodud PuckManid ringi liikuma 
     focus = (focus + 1) % 2;
     pacMen.forEach((item) => {
             checkCollisions(item);
@@ -42,13 +45,13 @@ function update() {
     setTimeout(update, 125);
 }
 
-function checkCollisions(item) {
+function checkCollisions(item) { // Funktsioon kontrollib, kas sein on juhuslikult ees, kui on ees siis pöörab tagasi
     let edgeW = window.innerWidth - item.img.width; let edgeH = window.innerHeight -item.img.width;
     if (item.position.x >= edgeW){item.velocity.x = item.velocity.x * -1; item.direction = 1;}
     if (item.position.x <=0){item.velocity.x = item.velocity.x * -1; item.direction = 0;}
     if (item.position.y >= edgeH || item.position.y <=0){item.velocity.y = item.velocity.y * -1;}
 }
 
-function makeOne() {
+function makeOne() { // Funktsioon kutsub välja teise funktsiooni, mis teeb PuckMane
 pacMen.push(makePac()); 
 }

323f2218502982ff30340d9262f349994da3bc06
rmirma <115686470+rmirma@users.noreply.github.com>
2022-12-04 15:33:14 +0200

    comment
    Fail kommenteeritud

--- js/mario.js
+++ js/mario.js
@@ -1,2 +1,2 @@
-$('#mäng').load('https://www.joshuakgoldberg.com/FullScreenMario/Source/ #game');
-$('#nupud').load('https://www.joshuakgoldberg.com/FullScreenMario/Source/ controls');
+$('#mäng').load('https://www.joshuakgoldberg.com/FullScreenMario/Source/ #game'); // Vastutab selle eest, et mängu ka mängida saaks.
+$('#nupud').load('https://www.joshuakgoldberg.com/FullScreenMario/Source/ controls'); // Vastutab selle eest, et mängule lisaks oleksid ka seadete muutmiseks kasutatavad nupud.
